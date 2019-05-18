bfef00cd3e017df7b8fdf288316debc3b8689965
ChauncyF <chauncyf@qq.com>
2019-05-18 02:03:02 -0400

    Update AddTwoNumbers.java

--- src/AddTwoNumbers.java
+++ src/AddTwoNumbers.java
@@ -23,21 +23,24 @@
 public class AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode res = new ListNode(0);
-        ListNode h1 = l1, h2 = l2, h3 = res;
-        int flag = 0;
-
-        while (h1 != null || h2 != null) {
-            int val1 = (h1 == null) ? 0 : h1.val;
-            int val2 = (h2 == null) ? 0 : h2.val;
-            int val = val1 + val2 + flag;
-            h3.next = new ListNode(val % 10);
-            h3 = h3.next;
-            flag = val / 10;
-            if (h1 != null) h1 = h1.next;
-            if (h2 != null) h2 = h2.next;
+        ListNode cur1 = l1, cur2 = l2, cur = res;
+        int payload = 0;
+        
+        while (cur1 != null || cur2 != null) {
+            if (cur1 != null) {
+                payload += cur1.val;
+                cur1 = cur1.next;
+            }
+            if (cur2 != null) {
+                payload += cur2.val;
+                cur2 = cur2.next;
+            }
+            cur.next = new ListNode(payload % 10);
+            cur = cur.next;
+            payload /= 10;
         }
-        if (flag != 0) {
-            h3.next = new ListNode(flag);
+        if (payload != 0) {
+            cur.next = new ListNode(payload);
         }
         return res.next;
     }
