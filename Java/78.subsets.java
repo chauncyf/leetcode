be0508b2292f0857d10a361031378389fe20a61b
chauncy <chauncyf@qq.com>
2019-08-05 23:18:53 -0500

    ..

---
+++ Java/78.subsets.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=78 lang=java
+ *
+ * [78] Subsets
+ *
+ * https://leetcode.com/problems/subsets/description/
+ *
+ * algorithms
+ * Medium (54.03%)
+ * Likes:    2156
+ * Dislikes: 54
+ * Total Accepted:    392.1K
+ * Total Submissions: 725.4K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Given a set of distinct integers, nums, return all possible subsets (the
+ * power set).
+ * 
+ * Note: The solution set must not contain duplicate subsets.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: nums = [1,2,3]
+ * Output:
+ * [
+ * ⁠ [3],
+ * [1],
+ * [2],
+ * [1,2,3],
+ * [1,3],
+ * [2,3],
+ * [1,2],
+ * []
+ * ]
+ * 
+ */
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        
+    }
+}
+
---
+++ Java/98.validate-binary-search-tree.java
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode id=98 lang=java
+ *
+ * [98] Validate Binary Search Tree
+ *
+ * https://leetcode.com/problems/validate-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (26.02%)
+ * Likes:    2148
+ * Dislikes: 323
+ * Total Accepted:    437.3K
+ * Total Submissions: 1.7M
+ * Testcase Example:  '[2,1,3]'
+ *
+ * Given a binary tree, determine if it is a valid binary search tree (BST).
+ * 
+ * Assume a BST is defined as follows:
+ * 
+ * 
+ * The left subtree of a node contains only nodes with keys less than the
+ * node's key.
+ * The right subtree of a node contains only nodes with keys greater than the
+ * node's key.
+ * Both the left and right subtrees must also be binary search trees.
+ * 
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * ⁠   2
+ * ⁠  / \
+ * ⁠ 1   3
+ * 
+ * Input: [2,1,3]
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 1   4
+ * / \
+ * 3   6
+ * 
+ * Input: [5,1,4,null,null,3,6]
+ * Output: false
+ * Explanation: The root node's value is 5 but its right child's value is 4.
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        Integer pre = null;
+        while (!stack.isEmpty() || cur != null) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            if (pre != null && pre >= cur.val) {
+                return false;
+            }
+            pre = cur.val;
+            cur = cur.right;
+        }
+        return true;
+    }
+}
+
